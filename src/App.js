0554aa15fcba86dea896d636f031f51a12ca0321
Andrew Jack <me@andrewjack.uk>
2018-08-04 23:51:58 +0100

    configure babel rc

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { StyleSheet, Text, View } from "react-native";
 
 type Props = {};
 
@@ -18,9 +11,7 @@ export class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to React Native Starter!</Text>
       </View>
     );
   }
