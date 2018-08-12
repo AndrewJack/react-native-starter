ca97722ce867d358e047fe5c5cdf5dbbff443f93
Andrew Jack <me@andrewjack.uk>
2018-08-12 10:37:02 +0100

    Add reanimated library

--- android/app/src/main/java/com/rnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnstarter/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new ReanimatedPackage(),
                     new RNGestureHandlerPackage()
             );
         }
