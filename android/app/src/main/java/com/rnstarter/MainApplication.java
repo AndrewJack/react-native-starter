934db5074e4048d93541ef823922f8bb5fbca8f3
Andrew Jack <me@andrewjack.uk>
2018-08-12 18:34:37 +0100

    Add RN firebase

--- android/app/src/main/java/com/rnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnstarter/MainApplication.java
@@ -13,6 +13,12 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.perf.RNFirebasePerformancePackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -29,8 +35,13 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
+            return Arrays.asList(
                     new MainReactPackage(),
+                    new RNFirebasePackage(),
+                    new RNFirebaseAnalyticsPackage(),
+                    new RNFirebaseCrashlyticsPackage(),
+                    new RNFirebasePerformancePackage(),
+                    new RNFirebaseRemoteConfigPackage(),
                     new CodePush("deployment-key-here", getApplicationContext(), BuildConfig.DEBUG),
                     new RNGestureHandlerPackage()
             );
