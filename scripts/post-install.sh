d6e600daa440fa8e5e769b92f22c2dd8af7e824f
Andrew Jack <me@andrewjack.uk>
2018-09-18 23:29:07 +0100

    Upgrade to RN 0.57 & update other dependencies (#4)

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 
 # Apply changes after yarn install
-
-echo "Copying missing RNScreens podspec"
-ORIGIN_DIR="scripts/patch-files"
-RNSCREENS_PODSPEC="RNScreens.podspec"
-cp -r "$ORIGIN_DIR/$RNSCREENS_PODSPEC" "node_modules/react-native-screens/$RNSCREENS_PODSPEC" 
