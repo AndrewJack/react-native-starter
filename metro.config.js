9db7e5d52bfea75f6a4cdd68fc1946cfb8905b89
Andrew Jack <me@andrewjack.uk>
2018-09-24 23:50:18 +0100

    indexed-ram-bundle doesn't work :(

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,22 @@
-module.exports = {
+const modulePaths = require('./modulePaths');
+const resolve = require('path').resolve;
+const fs = require('fs');
+
+const config = {
   transformer: {
     getTransformOptions: () => {
+      const moduleMap = {};
+      modulePaths.forEach(path => {
+        if (fs.existsSync(path)) {
+          moduleMap[resolve(path)] = true;
+        }
+      });
       return {
-        transform: { inlineRequires: true },
+        preloadedModules: moduleMap,
+        transform: { inlineRequires: { blacklist: moduleMap } },
       };
     },
   },
 };
+
+module.exports = config;
